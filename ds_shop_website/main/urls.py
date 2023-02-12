6524cbcd659c45afc7faa55a6891dbe71fb8de87
Rez1ay <aleksandr-titov-2002@mail.ru>
2023-02-12 10:12:46 +0300

    [upd9] login user

--- ds_shop_website/main/urls.py
+++ ds_shop_website/main/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
-    path('reg', views.RegUser.as_view(), name='reg')
+    path('reg', views.RegUser.as_view(), name='reg'),
+    path('login', views.LoginUser.as_view(), name='login')
 ]
--- ds_shop_website/main/views.py
+++ ds_shop_website/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import *
 
 
@@ -16,3 +18,11 @@ class RegUser(CreateView):
     form_class = RegUserForm
     template_name = 'main/reg.html'
     success_url = reverse_lazy('home')
+
+
+class LoginUser(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'main/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('home')
